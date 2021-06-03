7a4be8b46e11025b141b6fef0e4c50651a804c5e
Your Name <you@example.com>
2021-06-03 01:50:10 -0700

    added new pages for this page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,34 +1,4 @@
-// import classes from "./Home.module.css"
-// const Home = () => {
-//     return (
-//         <div className={classes.Home}>
-//             <section>
-//                 <div className={classes.paragraph}>
-//                     <h1>products company.</h1>
-//                     <p>
-//                        Also see How to Write an About Us Page for information about creating a story for your website, which you can use in your company presentation.
-//                   </p>
-//                     <button className={classes.buttonHome}>sign up</button>
-//                 </div>
-//             </section>
-//             <footer>
-//                 <div>
-//                     <span>
-//                         <p>Services</p>
-//                         <p>About Us</p>
-//                         <p>our company</p>
-//                         <p>Contacts</p>
-//                     </span>
-//                 </div>
 
-//                 <span className={classes.footerSpan}>
-//                     ©2021 Jackets shop
-//                 </span>
-//             </footer>
-//         </div>
-//     )
-// }
-// export default Home;
 
 import classes from "./Home.module.css"
 const Home = () => {
@@ -42,7 +12,7 @@ const Home = () => {
     <p>Legal addrress:722200,  Каракол ш,  Гебзе к, 124, телефон 0 (3922) 5-32-77; факс 0 (3922) 5-00-13, е-mail: issyk-kul_telecom@kt.kg</p>
         </header>
 
-    <footer>Asource:<a> http://kt.kg/about_us/contact/issyk_kul_branch1/</a>
+    <footer>Asource: http://kt.kg/about_us/contact/issyk_kul_branch1/
 ОАО КыргызТелеком</footer>
     </div> );
 }
